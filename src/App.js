9407155ce1e2240dc62e744e051a88c0d8612f72
Felipe Cunha <feliperuben@live.com>
2020-09-16 12:11:13 -0300

    remaking all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import LandingPage from './components/views/LandingPage/index'
-import LandingManage from './components/views/LandingPage/LandingManage/index'
-import LandingPhone from './components/views/LandingPage/LandingPhone/index'
+
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
-      <LandingManage />
-      <LandingPhone />
     </div>
   );
 }
