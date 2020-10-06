5c124031c978b96d5514f1be736d00c0efdbd9c7
Felipe Cunha <feliperubenmv@gmail.com>
2020-10-06 14:52:52 -0300

    Footer

--- src/components/views/LandingPage/3Landing/index.js
+++ src/components/views/LandingPage/3Landing/index.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 
 
-function Landing3() {
+const useStyles = makeStyles ({
+  root: {
+      "& .MuiBottomNavigationAction-root": {
+          minWidth: 0,
+          maxWidth: 250,
+      },
+      "& .MuiSvgIcon-root": {
+          fill: "tan",
+          "&:hover": {
+              fill: "tomato",
+              fontSize: "1.8ren",
+          },
+      },
+  },
+});
+
+const Footer = () => {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+          <BottomNavigation width="auto" style={{background: "#233"}}>
+              <BottomNavigationAction className={classes.root} style={{padding: 0}}
+              icon={<Facebook />} />
+              <BottomNavigationAction className={classes.root} style={{padding: 0}}
+              icon={<Twitter />} />
+              <BottomNavigationAction className={classes.root} style={{padding: 0}}
+              icon={<Instagram />} />
+          </BottomNavigation>
   );
 }
 
